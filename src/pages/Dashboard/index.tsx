1c5b8da696a8b1e8f23af9f15487fe7dffe31aea
Gubmt <gustavobarbieri@ymail.com>
2020-05-04 15:50:51 -0300

    feature:Challenge

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { format } from 'date-fns';
 
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
@@ -30,14 +31,41 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
-    }
+      const response = await api.get('/transactions');
 
+      const formattedTransactions = response.data.transactions.map(
+        (transaction: Transaction) => {
+          return {
+            id: transaction.id,
+            title: transaction.title,
+            value: transaction.value,
+            formattedValue: formatValue(transaction.value),
+            formattedDate: format(
+              new Date(transaction.created_at),
+              'dd/MM/yyyy',
+            ),
+            type: transaction.type,
+            category: { title: transaction.category.title },
+            created_at: transaction.created_at,
+          };
+        },
+      );
+      setTransactions(formattedTransactions);
+
+      const formattedIncome = formatValue(response.data.balance.income);
+      const formattedOutcome = formatValue(response.data.balance.outcome);
+      const formattedTotal = formatValue(response.data.balance.total);
+      setBalance({
+        income: formattedIncome,
+        outcome: formattedOutcome,
+        total: formattedTotal,
+      });
+    }
     loadTransactions();
   }, []);
 
@@ -51,21 +79,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -80,20 +108,21 @@ const Dashboard: React.FC = () => {
               </tr>
             </thead>
 
-            <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
-            </tbody>
+            {transactions &&
+              transactions.map(transaction => (
+                <tbody key={transaction.id}>
+                  <tr>
+                    <td className="title">{transaction.title}</td>
+                    <td className={transaction.type}>
+                      {transaction.type === 'outcome'
+                        ? `- ${transaction.formattedValue}`
+                        : transaction.formattedValue}
+                    </td>
+                    <td>{transaction.category.title}</td>
+                    <td>{transaction.formattedDate}</td>
+                  </tr>
+                </tbody>
+              ))}
           </table>
         </TableContainer>
       </Container>
